62e92625fba5e6741917bc0cccfddc0647359ab8
Aditya Narayan Jaiswal <nlaxmi886@gmail.com>
2024-01-09 17:33:53 +0530

    feat(homepage): Adding the homepage feature in uno+ game

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import LoginSignUp from './pages/LoginSignUpPage';
+import GamePage from './pages/GamePage';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" Component={LoginSignUp} />
-
+        <Route path='/game' Component={GamePage} />
       </Routes>
     </div>
   );
